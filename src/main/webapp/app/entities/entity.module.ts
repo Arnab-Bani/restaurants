f8684a698cbc5467979c5fe2a3a43f0037e9721f
sriyaz <riyaz.java83@gmail.com>
2017-03-02 21:48:32 +0530

    Added Entity for Category both server side and client side

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { FoodClientModule } from './client/client.module';
+import { FoodCategoryModule } from './category/category.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         FoodClientModule,
+        FoodCategoryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
